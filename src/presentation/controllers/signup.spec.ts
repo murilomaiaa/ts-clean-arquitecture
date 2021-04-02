d50d1246a519c0c8367a5b12ef06a3bd7b570384
Murilo Maia <murilomaia.bb@gmail.com>
2021-04-02 15:58:24 -0300

    test: ensure SignUpController returns 400 if passwor and passwordConfirmation doesnt match

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -79,6 +79,21 @@ describe('SignUp Controller', () => {
     expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation'))
   })
 
+  it('Should return 400 if confirmationPassword is not equal password', () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'anyemail@example.com',
+        name: 'Any Name ',
+        password: 'password',
+        passwordConfirmation: 'invalidConfirmation'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new InvalidParamError('passwordConfirmation'))
+  })
+
   test('Should return 400 if an invalid email is provided', () => {
     const { sut, emailValidatorStub } = makeSut()
     jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)

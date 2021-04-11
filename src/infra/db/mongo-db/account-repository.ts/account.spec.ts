1bcbc04a058ad18309d5bd9a2c3a1aa6518c4580
Murilo Maia <murilomaia.bb@gmail.com>
2021-04-11 16:23:20 -0300

    refactor: move sut creation to a factory method

--- src/infra/db/mongo-db/account-repository.ts/account.spec.ts
+++ src/infra/db/mongo-db/account-repository.ts/account.spec.ts
@@ -1,6 +1,10 @@
 import { MongoHelper } from '../helpers/mongo-helper'
 import { AccountMongoRepository } from './account'
 
+const makeSut = (): AccountMongoRepository => {
+  return new AccountMongoRepository()
+}
+
 describe('Account mongo repositiry', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
@@ -11,7 +15,7 @@ describe('Account mongo repositiry', () => {
   })
 
   it('Should return an account on success', async () => {
-    const sut = new AccountMongoRepository()
+    const sut = makeSut()
 
     const account = await sut.add({
       name: 'any-name',

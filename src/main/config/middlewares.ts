e07cc7223395e2800b3d96d2bee72ad969eff2ab
Murilo Maia <murilomaia.bb@gmail.com>
2021-04-25 14:44:14 -0300

    feat: enable cors

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,7 +1,9 @@
 import { INestApplication } from '@nestjs/common'
 import { contentType } from '../middlewares/contentType'
+import { cors } from '../middlewares/cors'
 
 export default (app: INestApplication) => {
   app.use(contentType)
+  app.use(cors)
   return app
 }
---
+++ src/main/middlewares/cors.ts
@@ -0,0 +1,6 @@
+export const cors = (req: any, res: any, next: () => void) => {
+  res.set('access-control-allow-origin', '*')
+  res.set('access-control-allow-headers', '*')
+  res.set('access-control-allow-methods', '*')
+  next()
+}

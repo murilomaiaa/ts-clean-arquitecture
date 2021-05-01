7ff69f27c80c3ec1e2b773919a86b7f5549c92d6
Murilo Maia <murilomaia.bb@gmail.com>
2021-05-01 20:45:40 -0300

    feat: connect to mongo before app listen

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import { MongoHelper } from './infra/db/mongo-db/helpers/mongo-helper'
 import { makeApp } from './main/config/app.module'
+import env from './main/config/env'
 
 async function bootstrap(): Promise<void> {
   const app = await makeApp()
-  await app.listen(3000)
+  MongoHelper.connect(env.mongoUrl)
+    .then(async () => await app.listen(env.port))
+    .then(() => console.log(`server started at http://localhost:${env.port}`))
+    .catch(console.log)
 }
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
--- src/main/config/app.module.ts
+++ src/main/config/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { SignUpRoute } from '../routes/signUp'
 import setupMiddlewares from './middlewares'
-
 @Module({
   controllers: [SignUpRoute]
 })

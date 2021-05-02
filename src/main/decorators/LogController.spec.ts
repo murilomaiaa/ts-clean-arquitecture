65ad4a91cff0033c233a4c9b2e3d354ae8010148
Murilo Maia <murilomaia.bb@gmail.com>
2021-05-02 11:42:17 -0300

    test: ensure LogControllerDecorator calls LogErrorRepository if controller returns a server error

--- src/main/decorators/LogController.spec.ts
+++ src/main/decorators/LogController.spec.ts
@@ -1,3 +1,5 @@
+import { LogErrorRepository } from '../../data/protocols/LogErrorRepository'
+import { serverError } from '../../presentation/helpers/httpHelper'
 import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
 import { LogControllerDecorator } from './LogController'
 
@@ -13,11 +15,21 @@ const makeController = () => {
   return new ControllerStub()
 }
 
+const makeLogError = () => {
+  class LogErrorRepositoryStub implements LogErrorRepository {
+    async log(stack: string): Promise<void> {
+
+    }
+  }
+  return new LogErrorRepositoryStub()
+}
+
 const makeSut = () => {
   const controllerStub = makeController()
-  const sut = new LogControllerDecorator(controllerStub)
+  const logErrorRepositoryStub = makeLogError()
+  const sut = new LogControllerDecorator(controllerStub, logErrorRepositoryStub)
 
-  return { sut, controllerStub }
+  return { sut, controllerStub, logErrorRepositoryStub }
 }
 
 describe('LogController', () => {
@@ -53,5 +65,29 @@ describe('LogController', () => {
       statusCode: 200,
       body: {}
     })
+  })
+
+  it('Should call LogErrorRepository with correct error if controller returns a server error', async () => {
+    const { sut, controllerStub, logErrorRepositoryStub } = makeSut()
+
+    const fakeError = new Error()
+    fakeError.stack = 'any_stack'
+    const error = serverError(fakeError)
+    jest.spyOn(controllerStub, 'handle')
+      .mockResolvedValueOnce(error)
+
+    const logSpy = jest.spyOn(logErrorRepositoryStub, 'log')
+
+    const httpRequest = {
+      body: {
+        email: 'any@mail.com',
+        name: 'Name',
+        password: '123456',
+        passwordConfirmation: '123456'
+      }
+    }
+
+    await sut.handle(httpRequest)
+    expect(logSpy).toHaveBeenCalledWith('any_stack')
   })
 })

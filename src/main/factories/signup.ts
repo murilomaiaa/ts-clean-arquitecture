d6fdff4566d76c05554196aefc594be6b9cfb98a
Murilo Maia <murilomaia.bb@gmail.com>
2021-05-02 16:17:14 -0300

    refactor: remove .ts from folder name

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,6 +1,6 @@
 import { DbAddAccount } from '../../data/usecases/addAccount/DbAddAccount'
 import { BcryptAdapter } from '../../infra/criptography/BcryptAdapter'
-import { AccountMongoRepository } from '../../infra/db/mongo-db/account-repository.ts/account'
+import { AccountMongoRepository } from '../../infra/db/mongo-db/account-repository/account'
 import { SignUpController } from '../../presentation/controllers/signup/signup'
 import { Controller } from '../../presentation/protocols'
 import { EmailValidatorAdapter } from '../../utils/EmailValidatorAdapter'
